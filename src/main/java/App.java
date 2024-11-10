ea1a9644cfc9b0c373d1d65a4a1c43cc49a70683
Denis Morozov <103926466+denfreeze@users.noreply.github.com>
2024-11-10 10:48:08 +0300

    version from 10 Nov

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,18 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean.getMessage());
+
+        Cat catBean = (Cat) applicationContext.getBean("cat");
+        System.out.println(catBean.getCatName());
+
+        Cat catBean2 = (Cat) applicationContext.getBean("cat");
+        System.out.println(catBean.getCatName());
+
+        System.out.println(bean == bean2);
+        System.out.println(catBean == catBean2);
     }
 }
---
+++ src/main/java/Cat.java
@@ -0,0 +1,17 @@
+public class Cat {
+    private String name;
+
+    public Cat() {};
+
+    public Cat(String name) {
+        this.name = name;
+    }
+
+    public String getCatName() {
+        return name;
+    }
+
+    public void setCatName(String name) {
+        this.name = name;
+    }
+}
